b4d26c7e0decc4e6670476f1ce78932a1b0f52ea
Rail' Sabirov <railich@bk.ru>
2011-06-11 07:42:07 +0400

    minimize requests, add includes on articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,8 @@ class Article < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :votes, :dependent => :destroy
 
-  scope :above_a_certain_rating, lambda{|minimum_rating| where('rating > ?',minimum_rating).order ('DATE(created_at) DESC, rating DESC')}
-  scope :below_a_certain_rating, lambda{|minimum_rating| where('rating <= ?',minimum_rating).order('DATE(created_at) DESC, rating DESC')}
+  scope :above_a_certain_rating, lambda{|minimum_rating| where('rating > ?',minimum_rating).order('DATE(created_at) DESC, rating DESC').includes([:category, :comments])}
+  scope :below_a_certain_rating, lambda{|minimum_rating| where('rating <= ?',minimum_rating).order('DATE(created_at) DESC, rating DESC').includes([:category, :comments])}
 
   def vote!(user_ip)
     self.votes.create!(:user_ip => user_ip, :user_identity => (self.id.to_s + '#' + user_ip.to_s))
